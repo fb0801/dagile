b36e732edc7488351d1fd9ce155b5ad19d12ca82
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2022-09-27 19:22:21 +0100

    Add files via upload

---
+++ SMEVSL.py
@@ -0,0 +1,21 @@
+'''Gather company data more quickly'''
+
+#modules for application use
+import re
+from bs4 import BeautifulSoup
+import requests
+
+
+def search():
+    search_term = input('what would you like to search  for? ') # input from user
+
+    #link we want to use
+    url = f"https://find-and-update.company-information.service.gov.uk/search?q={search_term}"
+
+
+
+
+
+if __name__ == "__main__":
+    #takes user to the main menu using magic method/dunders
+    search()
