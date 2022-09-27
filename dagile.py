deb19ca521cb8491b146bfce14315239e1759068
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2022-09-27 18:57:01 +0100

    Add files via upload

--- dagile.py
+++ dagile.py
@@ -1,10 +1,20 @@
+'''Python script to read two files and see if they are there otherwise add them'''
 
 def read_file_one():
-    with open("", 'r') as f:
+   with open("", 'r') as f:
+        for line in f:
+            if "" in line:
+            # found = True # Not necessary
+                return True
+        return False 
 
 
 
 def read_file_two():
     with open("", 'r') as g:
+        pass
 
 
+def write_file():
+    with open("", 'w+') as h:
+        pass 
