e586d7fd39ad0341d2ae87976a06e4cabeefb9de
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2022-09-27 18:58:46 +0100

    Add files via upload

--- email.py
+++ email.py
@@ -1,20 +1,48 @@
+#modules to use for application
+from calendar import month
 from datetime import datetime
 import pandas as pd
 
-  
-today = datetime.now()
+def getMonth():
+    #function to get current month
+    global month1
     
-month1 = today.strftime("%b")
-print("Current Month Full Name:", month1);
+    today = datetime.now()
+    month1 = today.strftime("%b")
+    print("Current Month Full Name:", month1)
 
 
-work = pd.read_excel("", usecols=['ID', 'First Name', 'Last Name', 'Email Address', 'START DATE', 'Org.', 'Gender', 'End DATE','Completer/Early Leaver', 'Folllow up Progression in work '])
+def main():
+    getMonth()
+    '''usecols=['ID', 'First Name', 'Last Name', 'Email Address', 
+    'START DATE', 'Org.', 'Gender', 'END DATE','Completer/Early Leaver', 'Folllow up Progression in work '])'''
+    
+    work = pd.read_excel("spreadsheet.xlsx", usecols=['ID', 'First Name', 'Email Address', 
+    'START DATE', 'Org.', 'Gender', 'END DATE', 'Exit questionnaire sent','Exit questionnarie received', 
+    'Folllow up Progression in work'])
+    print(work)
+
+    work[''].where(work[''] =="")
+        
 
 
-print(work)
 
 
 
-#function to send emails
 def SendEmail():
+    #function to send emails
     pass
+
+def exit_quest():
+    #exit questionnaire
+    pass
+
+
+def work_pro():
+    #work progress
+    pass
+
+
+if __name__ == "__main__":
+    #takes user to the main menu using magic method/dunders
+    main()
