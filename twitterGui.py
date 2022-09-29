7dcdd5c68ea4bb4203dd81b670909a44e18418b9
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2022-09-29 19:58:11 +0100

    Add files via upload

--- twitterGui.py
+++ twitterGui.py
@@ -1,6 +1,7 @@
 '''twitter bot gui version'''
 
 #modules to use
+from tkinter import messagebox
 from tkinter import *
 import re
 from bs4 import BeautifulSoup
@@ -10,26 +11,26 @@ import os
 
 
 class TwitterBot:
-    def __init__(self, ):
+    def __init__(self, master):
         #Frame.__init__(self, root) # a frame to contain everything in
-        root.title('Twitter Bot')
+        self.master = master
+        master.title('Twitter Bot')
         #self.grid() # to say we are using grids
-        self.speak() # to call the screen method
-    
+        #speak() # to call the screen method
 
-
-    def speak(self):
-       
         user_input = Entry(textvariable = name_input)
         user_input.pack()
         button_quit =Button(root, text="Exit program", command=root.quit)
         button_quit.pack()
-        help_btn = Button( text = '?',font = 'Times 20 ',width = 1, height = 1, command = self.helpbtn)
+        help_btn = Button(self, text = '?',font = 'Times 20 ',width = 1, height = 1, command = TwitterBot.bothelp)
         help_btn.grid(row = 8, column = 9)#pack(side = RIGHT)
+
+
+    
         #self.help_btn = Button(self, text = '?',font = 'Times 20 ',width = 1, height = 1, command = self.helpbtn)
         #self.help_btn.grid(row = 8, column = 9)#pack(side = RIGHT)
 
-    def helpbtn(self):
+    def bothelp(self):
         self.messagebox.showinfo("Help", "Enter your login details to access the To Do List") # if the user clicks on help this messagebox will appear 
 
     def botSearch(self):
