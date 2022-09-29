d2de08ceb6ef0114248696249554e2f68c7a1d37
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2022-09-29 19:40:58 +0100

    lets try again

--- twitterGui.py
+++ twitterGui.py
@@ -9,32 +9,36 @@ import json
 import os
 
 
-class TwitterBot(Frame):
-    def __init__(self, master):
-        Frame.__init__(self, master) # a frame to contain everything in
-        self.grid() # to say we are using grids
+class TwitterBot:
+    def __init__(self, ):
+        #Frame.__init__(self, root) # a frame to contain everything in
+        root.title('Twitter Bot')
+        #self.grid() # to say we are using grids
         self.speak() # to call the screen method
+    
 
 
-        
+    def speak(self):
+       
+        user_input = Entry(textvariable = name_input)
+        user_input.pack()
+        button_quit =Button(root, text="Exit program", command=root.quit)
+        button_quit.pack()
+        help_btn = Button( text = '?',font = 'Times 20 ',width = 1, height = 1, command = self.helpbtn)
+        help_btn.grid(row = 8, column = 9)#pack(side = RIGHT)
+        #self.help_btn = Button(self, text = '?',font = 'Times 20 ',width = 1, height = 1, command = self.helpbtn)
+        #self.help_btn.grid(row = 8, column = 9)#pack(side = RIGHT)
 
-def helpbtn(self):
-    self.messagebox.showinfo("Help", "Enter your login details to access the To Do List") # if the user clicks on help this messagebox will appear 
+    def helpbtn(self):
+        self.messagebox.showinfo("Help", "Enter your login details to access the To Do List") # if the user clicks on help this messagebox will appear 
 
+    def botSearch(self):
+        pass
 
-
-def speak(self):
-    self.Entrybox = Entry(textvariable = name_input)
-    button_quit =Button(root, text="Exit program", command=root.quit)
-    button_quit.pack()
-    help_btn = Button( text = '?',font = 'Times 20 ',width = 1, height = 1, command = self.helpbtn)
-    help_btn.grid(row = 8, column = 9)#pack(side = RIGHT)
-    self.help_btn = Button(self, text = '?',font = 'Times 20 ',width = 1, height = 1, command = self.helpbtn)
-    self.help_btn.grid(row = 8, column = 9)#pack(side = RIGHT)
 
 root = Tk()
 name_input = StringVar()
 
 
-root.title('Twitter Bot')
+Terry = TwitterBot(root)
 root.mainloop()
