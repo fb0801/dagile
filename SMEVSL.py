729bc51f62518bda89ca329ddffbcc10dabdfcdb
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2022-10-09 19:53:41 +0100

    Add files via upload

--- SMEVSL.py
+++ SMEVSL.py
@@ -7,14 +7,23 @@ import requests
 import json
 import os
 
+items_found = {}
+
 def search():
     search_term = input('what would you like to search  for? ') # input from user
 
     #link we want to use
     url = f"https://find-and-update.company-information.service.gov.uk/search?q={search_term}"
+    page = requests.get(url).text
+    doc= BeautifulSoup(page, 'html.parser')
 
+    page_text = doc.find(class_="type-company")#results-list
+    items_found[page_text]
+    for items in items_found:
+        print(items)
 
-
+def searchResults():
+    pass
 
 
 if __name__ == "__main__":
