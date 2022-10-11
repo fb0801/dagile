de43d3588d94c059fcd2f5a50d5d9cb261f44561
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2022-10-11 20:11:57 +0100

    Add files via upload

--- farhanfilter.py
+++ farhanfilter.py
@@ -1,4 +1,8 @@
-'''application to fix the file'''
+'''application to fix the file 
+part of https://github.com/fb0801/dagile created by Farhan
+
+
+'''
 
 #application modules
 import pandas as pd
@@ -6,14 +10,26 @@ import numpy as np
 
 
 
-work = pd.read_excel("STUDENTS_spreadsheet.xlsx") 
+work = pd.read_excel("STUDENTS_test.xlsx") 
 
 
 def main():
     newfile = work.drop(columns=['Progress','Last log in Dagile UK'])
+    work['Last Name'] = work['Last Name'].str.replace("''", "")
+    #work['Last Name'] = work['Last Name'].strip("'")
 
-    newfile.to_csv('file_name.csv') 
+    #newfile.loc[work['Last Name'].isin(['Last Name'])] = ''
+    name = input("Enter name of file: ")
+    newfile.to_csv(f'{name}.csv') 
     print("task completed")
+
+
+'''
+Remove the square brackets and quotation marks from the ‘groups’ column by highlighting the column, pressing ctrl + F, 
+search for [ , leave the ‘replace with’ field blank then select ‘replace all’. Repeat for ] and “:
+'''
+
+
 
 if __name__ == "__main__":
     #takes user to the main menu using magic method/dunders
