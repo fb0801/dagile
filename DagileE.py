6b1c47c33b0f48163d079f5c113eab26541ddd91
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-01-11 18:36:02 +0000

    Add files via upload

---
+++ DagileE.py
@@ -0,0 +1,4 @@
+import smtplib, ssl, csv
+import pandas as pd
+import numpy as np
+import os
---
+++ Dagileff.py
@@ -0,0 +1,13 @@
+'''application to fix the file 
+part of https://github.com/fb0801/dagile created by Farhan Bhatti
+
+Remove the square brackets and quotation marks from the ‘groups’ column
+search for [ , replace all’. Repeat for ] and “:
+
+'''
+
+#application modules
+import pandas as pd
+import numpy as np
+import os
+import tkinter
